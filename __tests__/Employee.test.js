f5c4108f7a20ac281783897b4fc4da4e78a7ad1a
lucasmo066 <lucasmo066@gmail.com>
2022-01-16 13:26:55 -0500

    passing employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,31 +3,31 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', ()=>{
     const employee = new Employee('Lucas Moraes', 23, 'lucasmo23');
 
-    expect(employee.Name).toBe('Mark');
+    expect(employee.Name).toBe('Lucas');
     expect(employee.ID).toEqual(expect.any(Number));
     expect(employee.Email).toEqual(expect.any(String));
 });
 
 test('gets employees name', ()=>{
-    const employee = new Employee('Mark');
+    const employee = new Employee('Lucas');
 
     expect(employee.getName()).toEqual(expect.stringContaining(employee.Name.toString()));
 })
 
 test('gets employees id', ()=>{
-    const employee = new Employee('Mark', 78);
+    const employee = new Employee('Lucas', 23);
 
     expect(employee.getId()).toEqual(expect.stringContaining(employee.ID.toString()));
 })
 
 test('gets employees email', ()=>{
-    const employee = new Employee('Mark', 78, 'Mark7');
+    const employee = new Employee('Lucas', 23, 'lucasmo6');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.Email.toString()));
 })
 
 test('returns the role', ()=>{
-    const employee = new Employee('Mark')
+    const employee = new Employee('Lucas')
 
     expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
 })
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
 const inquirer = require('inquirer');
 const pageTemplate = require('./src/page-template');
 const{writeFile} = require('./utils/generate-HTML');
 const mock = require('./src/mock-data');
 
+// team profiles
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 let teamArray = [];
+
